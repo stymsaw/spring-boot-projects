dfcfd4afb54006a42f9b7bfb0ebcf424a358a5d0
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-02-07 18:02:30 +0530

    starting jpa-and-hibernate project

---
+++ jpa-and-hibernate/src/main/java/stym/springboot/jpaandhibernate/JpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package stym.springboot.jpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAndHibernateApplication.class, args);
+	}
+
+}
