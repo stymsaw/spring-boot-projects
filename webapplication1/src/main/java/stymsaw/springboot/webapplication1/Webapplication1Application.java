20f37714c3a3b98a0792140c54c1ac6234ad6463
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-02-08 17:35:20 +0530

    first commit

---
+++ webapplication1/src/main/java/stymsaw/springboot/webapplication1/Webapplication1Application.java
@@ -0,0 +1,13 @@
+package stymsaw.springboot.webapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapplication1Application.class, args);
+	}
+
+}
---
+++ webapplication1/src/test/java/stymsaw/springboot/webapplication1/Webapplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package stymsaw.springboot.webapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
