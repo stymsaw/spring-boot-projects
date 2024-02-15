5008ab9161ae403bf4eaec22a34261d991b985ba
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-02-15 17:07:30 +0530

    adding bean form backing

--- webapplication1/src/main/java/stymsaw/springboot/webapplication1/todos/TodoController.java
+++ webapplication1/src/main/java/stymsaw/springboot/webapplication1/todos/TodoController.java
@@ -38,9 +38,9 @@ public class TodoController {
 
 
     @RequestMapping(value="add-todo", method = RequestMethod.POST)
-    public String addNewTodo(@RequestParam String description, ModelMap model) {
+    public String addNewTodo(ModelMap model, Todo todo) {
         String username = (String)model.get("name");
-        todoService.addTodo(username, description,
+        todoService.addTodo(username, todo.getDescription(),
                 LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
--- webapplication1/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ webapplication1/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <html>
 <head>
